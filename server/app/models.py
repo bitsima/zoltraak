d3f7c3ab2a1f1b74b31be10e54fe85f56129ac57
bitsima <betulse42@gmail.com>
2024-07-09 14:30:59 +0300

    feat: add commandqueues for implants

--- server/app/models.py
+++ server/app/models.py
@@ -1,7 +1,12 @@
+from sqlalchemy.dialects.postgresql import JSONB
+
 from app import db
 
 
 class CommandQueue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     implant_uuid = db.Column(db.String, index=True)
-    command = db.Column(db.String)
+    commands = db.Column(JSONB)
+
+    def __repr__(self):
+        return f"<CommandQueue(id={self.id}, implant_uuid={self.implant_uuid}, commands={self.commands})>"
