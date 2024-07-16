3f5d3bf4d7e02bd99021b51f82144e6fea29b755
bitsima <betulse42@gmail.com>
2024-07-16 19:18:19 +0300

    feat: add FileChunk model

--- server/app/database/models.py
+++ server/app/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.dialects.postgresql import JSONB, BYTEA
 
 from app import db
 
@@ -10,3 +10,15 @@ class CommandQueue(db.Model):
 
     def __repr__(self):
         return f"<CommandQueue(id={self.id}, implant_uuid={self.implant_uuid}, commands={self.commands})>"
+
+
+class FileChunk(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    implant_uuid = db.Column(db.String, index=True)
+    file_id = db.Column(db.String, index=True)
+    chunk_index = db.Column(db.Integer, index=True)
+    chunk_data = db.Column(BYTEA)
+
+    def __repr__(self):
+        return f"<FileChunk(id={self.id}, implant_uuid={self.implant_uuid}, file_id={self.file_id}, \
+            chunk_index={self.chunk_index}, total_chunks={self.total_chunks})>"
